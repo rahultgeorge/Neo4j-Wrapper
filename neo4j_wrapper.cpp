d0bc6f92027fb16357cdc5c2bab59b6540469b9b
rahultgeorge <rahultitusgeorge@gmail.com>
2020-05-20 04:16:49 -0400

    PDG specific functions completed. Tested successfully

--- neo4j_wrapper.cpp
+++ neo4j_wrapper.cpp
@@ -1,59 +1,281 @@
 #include "neo4j_wrapper.hpp"
 
-int Neo4j_Wrapper::Neo4j_Connector::connect()
-{
-
+int Neo4j_Wrapper::Neo4j_Connector::connect() {
     connection = neo4j_connect("neo4j://neo4j:secret@localhost:7687", NULL, NEO4J_INSECURE);
-    if (connection == NULL)
-    {
+    if (connection == NULL) {
         neo4j_perror(stderr, errno, "Connection failed");
         return 0;
     }
- return 1;
+    return 1;
 }
 
-void Neo4j_Wrapper::Neo4j_Connector::close()
-{
-   neo4j_close(connection);
-   neo4j_client_cleanup();
+void Neo4j_Wrapper::Neo4j_Connector::close() {
+    neo4j_close(connection);
+    neo4j_client_cleanup();
 }
 
 
-int Neo4j_Wrapper::Neo4j_Connector::insert(const char* node_type,int num_arguments, char** arguments)
-{
-     // TODO - Add generic insert and create methods
+int Neo4j_Wrapper::Neo4j_Connector::insert(const char *node_type, int num_arguments, char **arguments) {
+    // TODO - Add generic insert and create methods
     return EXIT_SUCCESS;
 }
 
 
-int Neo4j_Wrapper::Neo4j_Connector::insert_pdg_edge(string node_source_type,string source_instruction, string edge_label, string node_dest_type, string dest_instruction)
-{
+int Neo4j_Wrapper::Neo4j_Connector::insert_pdg_edge(string node_source_label, string edge_label,
+                                                    string node_dest_label) {   // Only using label to find the nodes (assuming nodes have been created)
+    string statement =
+            "MERGE (p1:ProgramInstruction{label:\'" + node_source_label + "\'}) MERGE(p2:ProgramInstruction{label:\'" +
+            node_dest_label + "\'}) MERGE((p1)-[:EDGE{label:\'" + edge_label + "\'}]->(p2))";
+    neo4j_result_stream_t *results = neo4j_run(connection, statement.c_str(), neo4j_null);
 
-    string statement="MERGE (:ProgramInstruction{type:\'"+node_source_type+"\', instruction:\'"+ source_instruction+"\'})-[:EDGE{label:\'"+edge_label+"\'}]->(:ProgramInstruction{type:\'"+node_dest_type+"\', instruction:\'"+ dest_instruction+"\'})";
-    neo4j_result_stream_t *results = neo4j_run(connection,statement.c_str(),neo4j_null );
-    if (results == NULL)
-    {
+    if (results == NULL) {
         neo4j_perror(stderr, errno, "Failed to run statement");
         return 0;
     }
-    if(results==NULL)
-        neo4j_perror(stderr, errno, strerror(errno));
 
+    neo4j_close_results(results);
+    return 1;
+
+}
+
+
+int Neo4j_Wrapper::Neo4j_Connector::insert_pdg_node(string label, string node_type,
+                                                    string instruction) {//  std::string label;
+    string statement = "MERGE (n:ProgramInstruction{label:\'" + label + "\'}) ON MATCH SET n.type=\'" + node_type +
+                       "\', n.instruction=\'" + instruction + "\' ON CREATE SET n.type=\'" + node_type +
+                       "\', n.instruction=\'" + instruction + "\'";
+    neo4j_result_stream_t *results = neo4j_run(connection, statement.c_str(), neo4j_null);
+    if (results == NULL) {
+        neo4j_perror(stderr, errno, "Failed to run statement");
+        return 0;
+    }
     neo4j_close_results(results);
     return 1;
 }
 
 
-int Neo4j_Wrapper::Neo4j_Connector::insert_pdg_node(string label, string node_type,string instruction)
-{
-    //  std::string label;
-  string statement="CREATE (:ProgramInstruction{label:\'"+label+"\', type:\'"+node_type+"\', instruction:\'"+ instruction+"\'})";
-  neo4j_result_stream_t *results = neo4j_run(connection,statement.c_str(),neo4j_null );
-    if (results == NULL)
-    {
-        neo4j_perror(stderr, errno, "Failed to run statement");
-        return 0;
-    }
-  neo4j_close_results(results);
-  return 1;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- neo4j_wrapper.hpp
+++ neo4j_wrapper.hpp
@@ -8,32 +8,30 @@
 using namespace std;
 
 
-namespace Neo4j_Wrapper
-{
- class Neo4j_Connector
- {
-   private:
-            neo4j_connection_t *connection;
+namespace Neo4j_Wrapper {
+    class Neo4j_Connector {
+    private:
+        neo4j_connection_t *connection;
 
-   public:
+    public:
 
-            Neo4j_Connector()
-            {
-                  neo4j_client_init();
-            }
+        Neo4j_Connector() {
+            neo4j_client_init();
+        }
 
-            //Connect to the db (Return 1 on success)
-            int connect();
+        //Connect to the db (Return 1 on success)
+        int connect();
 
-            // Generic function to insert data
-            int insert(const char* node_type,int num_arguments, char** arguments);
+        // Generic function to insert data
+        int insert(const char *node_type, int num_arguments, char **arguments);
 
-            int insert_pdg_edge(string node_source_type,string source_instruction, string edge_label, string node_dest_type, string dest_instruction);
+        int insert_pdg_edge(string node_source_label, string edge_label,
+                            string node_dest_label);
 
-            int insert_pdg_node(string label,string node_type,string instruction);
+        int insert_pdg_node(string label, string node_type, string instruction);
 
-            //Close the connection and clean up
-            void close();
- };
+        //Close the connection and clean up
+        void close();
+    };
 
 }
